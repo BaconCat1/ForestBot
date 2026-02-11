526eaa09057ba5e9421b7ee129e32bd717ccf358
jollycurv-e <jollycurve77@gmail.com>
2026-02-11 12:38:38 -0700

    typos, more ignored files

--- src/commands/drop.ts
+++ src/commands/drop.ts
@@ -4,7 +4,7 @@ import type Bot from "../structure/mineflayer/Bot.js";
 
 export default {
     commands: ['drop'],
-    description: ` I will drop items in my hand, or all of my items. ${config.prefix}drop (all)(optional)`,
+    description: ` I will drop items in my hand, or all of my items. ${config.prefix}drop <all>(optional)`,
     minArgs: 0,
     maxArgs: 1,
     execute: async (user, args, bot: Bot, api: ForestBotAPI) => {
